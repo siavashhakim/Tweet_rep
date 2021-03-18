d3daaacd039e5bcb526be6f5e44dc10b3a766536
Siavash Hakim Elahi <30736316+siavashhakim@users.noreply.github.com>
2021-03-18 16:40:06 -0700

    Delete MYSQL_connection_test.py

--- MYSQL_connection_test.py
+++
@@ -1,6 +0,0 @@
-# tip: pip install mysql-connector-python and pip uninstall mysql-connector
-import mysql.connector
-password = 'Aa1367haksia13852020'
-
-con = mysql.connector.connect(host='localhost',
-                                      database='twitterdb', user='root', password=password,auth_plugin='mysql_native_password')
